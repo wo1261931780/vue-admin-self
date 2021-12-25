40ac51d6e1dc7e2a02e0e4820ab09d7a3a835c2a
junw-gitkraken <wo1261931780@gmail.com>
2021-12-25 23:34:48 +0800

    eslint problem

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,25 +1,30 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-12-23 23:46:29
+ * @LastEditTime: 2021-12-25 23:33:03
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \vue-admin-self\src\views\dashboard\index.vue
+-->
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">name: {{ name }}</div>
     <div>show me somthing</div>
     <!-- <el-alert :closable="false" title="menu 1-1" type="success"> </el-alert> -->
-    <el-button :plain="true" @click="open" class="s1"> show me </el-button>
-                <el-input
-                class="input-with-select"
-                change="querydemo"
-                placeholder="请输入内容,目前支持测试测试搜索(╯°Д°)╯︵┻━┻">
-                <el-select
-                    slot="prepend"
-                    
-                    class="el-selectdemo"
-                    placeholder="请选择">
-                    <el-option label="title" value="1"></el-option>
-                    <el-option label="keyword" value="2"></el-option>
-                    <el-option label="audioid" value="3"></el-option>
-                    <el-option label="coverdesignerid" value="4"></el-option>
-                </el-select>
-                <el-button slot="append" icon="el-icon-search" click="querydemo"></el-button>
-            </el-input>
+    <el-button :plain="true" class="s1" @click="open"> show me </el-button>
+    <el-input
+      class="input-with-select"
+      change="querydemo"
+      placeholder="请输入内容,目前支持测试测试搜索(╯°Д°)╯︵┻━┻"
+    >
+      <el-select slot="prepend" class="el-selectdemo" placeholder="请选择">
+        <el-option label="title" value="1" />
+        <el-option label="keyword" value="2" />
+        <el-option label="audioid" value="3" />
+        <el-option label="coverdesignerid" value="4" />
+      </el-select>
+      <el-button slot="append" icon="el-icon-search" click="querydemo" />
+    </el-input>
   </div>
 </template>
 <script src="//cdn.jsdelivr.net/npm/element-plus"></script>
@@ -29,19 +34,16 @@ import { mapGetters } from "vuex";
 // import { defineComponent, h } from "vue";
 
 export default {
-  name: 'Dashboard',
+  name: "Dashboard",
   computed: {
-    ...mapGetters([
-      'name'
-    ])
+    ...mapGetters(["name"]),
   },
-  methods:{
-    open(){
-      
-      this.$message("这是消息提示")
+  methods: {
+    open() {
+      this.$message("这是消息提示");
     },
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
