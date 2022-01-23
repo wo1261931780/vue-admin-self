560dc31103d2258fb835866879be93ae6d986b98
junw <45444154+wo1261931780@users.noreply.github.com>
2022-01-23 21:18:03 +0800

    1

--- src/views/jixiao/jixiao.vue
+++ src/views/jixiao/jixiao.vue
@@ -386,13 +386,10 @@
 </template>
 
 <script>
-// import { title } from '@/settings'
 export default {
 
   data() {
     return {
-    //   input1: '',
-    //   input2: '',
       input3: '',
       select: '',
       formInline: {
