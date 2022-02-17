25b7894fa4dc8c130cf2af526a9594824efa259f
junw <45444154+wo1261931780@users.noreply.github.com>
2022-02-17 13:36:26 +0800

    demo

--- build/index.js
+++ build/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2022-01-20 12:09:45
+ * @LastEditTime: 2022-02-15 16:00:17
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \vue-admin-self\build\index.js
+ */
 const { run } = require('runjs')
 const chalk = require('chalk')
 const config = require('../vue.config.js')
